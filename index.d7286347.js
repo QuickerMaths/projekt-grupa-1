e18d43af3d601f97dfaa2bc8d70e7b6494b48c89
QuickerMaths <116837090+QuickerMaths@users.noreply.github.com>
2023-02-01 19:40:40 +0000

    Deploying to gh-pages from @ QuickerMaths/projekt-grupa-1@8719722a71ed2a6e054aee2147b8c5b50afb03c2 🚀

---
+++ index.d7286347.js
@@ -0,0 +1,2 @@
+const openButton=document.querySelector(".modal-button"),closeButton=document.querySelector(".modal-contacts__button"),modal=document.querySelector(".overlay-contacts");openButton.addEventListener("click",(()=>{modal.classList.toggle("hidden")})),closeButton.addEventListener("click",(()=>{modal.classList.toggle("hidden")}));
+//# sourceMappingURL=index.d7286347.js.map
